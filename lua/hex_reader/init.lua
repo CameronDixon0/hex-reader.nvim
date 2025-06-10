d9a17cd34604b0ecd7a6f9bd4689d351d59c5ba3
CameronDixon0 <109122993+CameronDixon0@users.noreply.github.com>
2025-06-10 17:15:23 +1000

    open hex

--- lua/hex_reader/init.lua
+++ lua/hex_reader/init.lua
@@ -1,7 +1,15 @@
 local M = {}
 
-M.say_hello = function()
-  print("Hello from my plugin!")
+function M.setup(opts)
+  M.opts = vim.tbl_deep_extend("force", {
+    -- default options
+    show_ascii = true,
+    show_address = true,
+  }, opts or {})
+end
+
+function M.open()
+  vim.cmd('%!xxd -g 2')
 end
 
 return M
