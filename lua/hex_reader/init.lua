b0bd20aa19ec107994e3092ccba00706c5c2991f
CameronDixon0 <109122993+CameronDixon0@users.noreply.github.com>
2025-06-10 17:22:37 +1000

    updated open function

--- lua/hex_reader/init.lua
+++ lua/hex_reader/init.lua
@@ -9,7 +9,25 @@ function M.setup(opts)
 end
 
 function M.open()
+  -- run :%!xxd -g 2
   vim.cmd('%!xxd -g 2')
+
+  -- create a new namespace for your plugin's virtual text
+  local ns_id = vim.api.nvim_create_namespace("hex_reader")
+
+  -- get current buffer
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  -- get total line count
+  local line_count = vim.api.nvim_buf_line_count(bufnr)
+
+  -- add virtual text to each line
+  for i = 0, line_count - 1 do
+    vim.api.nvim_buf_set_extmark(bufnr, ns_id, i, -1, {
+      virt_text = { { "Hello world", "Comment" } },
+      virt_text_pos = "eol", -- put it at the end of the line
+    })
+  end
 end
 
 return M
